c92fc822e179f61b099467b581b7748378240eaf
elvinson <elvinmd@gmail.com>
2022-11-22 21:26:04 +0300

    настройка webpack + bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/elvinson/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/elvinson/frontend-project-lvl3/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/a599dd7b2b9f5af4c332/maintainability)](https://codeclimate.com/github/elvinson/frontend-project-lvl3/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true
+    },
+    entry: './src/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'rss reader',
+            template: './src/template.html'
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.(scss)$/,
+            use: [
+              {
+                loader: 'style-loader'
+              },
+              {
+                loader: 'css-loader'
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    plugins: () => [
+                      require('autoprefixer')
+                    ]
+                  }
+                }
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ]
+          }
+        ]
+    }
+};
